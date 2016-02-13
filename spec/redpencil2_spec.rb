fe26f7bb1dbb13a01198a0f15bed453ca27c2241
Lovinder Pnag <lspnag@gmail.com>
2016-02-13 12:24:32 -0500

    refactor helper methods

--- spec/redpencil2_spec.rb
+++ spec/redpencil2_spec.rb
@@ -32,12 +32,8 @@ describe 'Product' do
   let(:num) {double('num')}
   subject {Product.new(num, num)}
 
-  def discount_price discount, price
-    allow(subject).to receive_messages(discount: discount, price: price)
-  end
-
-  def price amount
-    allow(subject).to receive(:price).and_return(amount)
+  def enable method, result
+    allow(subject).to receive(method.to_sym).and_return(result)
   end
 
   it 'expect 2 arguments' do
@@ -46,29 +42,36 @@ describe 'Product' do
 
   describe '#percent_reduced' do
     it 'calculates percent reduction' do
-      discount_price 20, 50
+      enable('discount', 20); enable('price', 50)
       expect(subject.percent_reduced(num)).to eq 40.0
     end
   end
 
   describe '#discount' do
     it 'calculates the discount' do
-      price 80
+      enable('price', 80)
       expect(subject.discount(75)).to eq 5
     end
   end
 
   describe '#price_stable?' do
     it 'check stability of price' do
-      allow(subject).to receive(:stability).and_return(20)
+      enable('stability', 20)
       expect(subject.price_stable?).to be false
     end
   end
 
   describe '#on_sale?' do
     it 'check if product is on sale?' do
-      price 90
+      enable('price', 90)
       expect(subject.on_sale?(80)).to be true
+    end
+  end
+
+  describe '#stable_and_on_sale?' do
+    it 'figures if product is table and on sale' do
+      enable('price_stable?',true); enable('on_sale?',true)
+      expect(subject.stable_and_on_sale?(num)).to eq true
     end
   end
 end
