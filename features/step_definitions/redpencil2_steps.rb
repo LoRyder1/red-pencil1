a2588847cf86fa1fdc4d4dedee0a914837b2b951
Lovinder Pnag <lspnag@gmail.com>
2016-02-15 16:12:06 -0500

    transform word to boolean

--- features/step_definitions/redpencil2_steps.rb
+++ features/step_definitions/redpencil2_steps.rb
@@ -10,13 +10,8 @@ Then(/^the price should be set to (\d+)$/) do |sale_price|
   expect(@chair.promo_price).to eq sale_price
 end
 
-And(/^the promo should be (\w+)$/) do |boolean|
-  class String
-    def to_bool
-      self == 'true'
-    end
-  end
-  expect(@chair.valid?).to eq boolean.to_bool
+And(/^the promo should be (\S[a-z]*)$/) do |boolean|
+  expect(@chair.valid?).to eq boolean
 end
 
 # When(/^the promotion runs for (\d+) days$/) do |days|
--- features/step_definitions/transform.rb
+++ features/step_definitions/transform.rb
@@ -1,3 +1,13 @@
+class String
+  def to_bool
+    self == 'true'
+  end
+end
+
 Transform(/^(-?\d+)$/) do |number|
   number.to_i
+end
+
+Transform(/^(-?\S[a-z]*)$/) do |word|
+  word.to_bool
 end
